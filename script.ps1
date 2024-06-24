003713677c21c096c6c4805a5abc26c748272735
AKremzer <75171579+AKremzer@users.noreply.github.com>
2024-06-24 23:02:56 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,3 @@
-$OutputEncoding = [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("UTF-8")
-
 $targetPath = "C:\Windows\System32\calc.exe"
 $linkPath = "$env:APPDATA\$([char]0x0421)$([char]0x043F)$([char]0x0440)$([char]0x0430)$([char]0x0432)$([char]0x043A)$([char]0x0430).lnk"
 
