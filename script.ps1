a81414bdc7202ea9b1cc084b2f682e8344fda93b
AKremzer <75171579+AKremzer@users.noreply.github.com>
2024-06-25 12:10:47 +0700

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,5 @@
 $targetPath = "C:\Windows\System32\calc.exe"
-$linkPath = "$env:APPDATA\$([char]0x0421)$([char]0x043F)$([char]0x0440)$([char]0x0430)$([char]0x0432)$([char]0x043A)$([char]0x0430).lnk"
+$linkPath = "$env:APPDATA\Help.lnk"
 
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut($linkPath)
