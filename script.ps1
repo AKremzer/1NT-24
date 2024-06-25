acdb970ff070abf6830f23a63509f6779c0a8fb6
AKremzer <75171579+AKremzer@users.noreply.github.com>
2024-06-25 12:43:51 +0700

    Update script.ps1 final final

--- script.ps1
+++ script.ps1
@@ -1,8 +1,12 @@
 $targetPath = "C:\Windows\System32\calc.exe"
 $linkPath = "$env:APPDATA\Help.lnk"
 
+$wordRegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\Winword.exe"
+$wordPath = (Get-ItemProperty -Path $wordRegistryPath).'(default)'
+echo $wordPath
+
 $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut($linkPath)
-$shortcut.IconLocation = "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE, 0"
+$shortcut.IconLocation = "$wordpath, 0"
 $shortcut.TargetPath = $targetPath
 $shortcut.Save()
